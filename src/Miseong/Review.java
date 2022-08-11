f564d33ec42788ec9fa0ff2c5ede3e74a6a81183
Miseong Kang <anykms9595@gmail.com>
2022-08-11 20:55:57 +0900

    Initial commit

---
+++ src/Miseong/Review.java
@@ -0,0 +1,9 @@
+package Miseong;
+
+public class Review {
+    public static void sayName(){
+        System.out.println("My name is Miseong");
+    }
+    public static void sayAge(){
+        System.out.println(26);
+    }}
