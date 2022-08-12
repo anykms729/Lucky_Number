d358ba40d0924732c78ddc9ddc704e278c0f1947
Miseong <anykms9595@gmail.com>
2022-08-13 00:54:39 +0900

    Delete Review.java

--- src/Miseong/Review.java
+++
@@ -1,9 +0,0 @@
-package Miseong;
-
-public class Review {
-    public static void sayName(){
-        System.out.println("My name is Miseong");
-    }
-    public static void sayAge(){
-        System.out.println(26);
-    }}
